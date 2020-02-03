7bc6fd19937c3860c6920bb78eb33e1c4df22408
Evan Yang <evanyang@pop-os.localdomain>
2020-02-03 15:18:13 -0800

    added fetch api, save city

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const kelvinToFarenheit = (kelvin) => {
+  return (kelvin - 273.15) * 9 / 5 + 32
+}
+
+// list saved cities (local storage)
+if ( JSON.parse(localStorage.getItem('cities'))) {
+  let initialCities = JSON.parse(localStorage.getItem('cities')) || localStorage.getItem('cities')
+  initialCities.forEach(city => {
+    let cityElem = document.createElement('li')
+    cityElem.textContent = city
+    document.getElementById('saved-cities-list').append(cityElem)
+  });
+}
+
 document.getElementById('city-submit').addEventListener('click', event => {
   event.preventDefault()
   let city = document.getElementById('city-input').value
@@ -6,5 +20,25 @@ document.getElementById('city-submit').addEventListener('click', event => {
     .then(r => r.json())
     .then(res => {
       console.log(res)
+      let {temp} = res.main, {name} = res
+      document.getElementById('city').textContent = name
+      //console.log(res.weather[0.description])
+      let description = res.weather[0].description 
+      document.getElementById('temperature').textContent = `Temperature (F): ${kelvinToFarenheit(temp).toFixed(2)}`
+      document.getElementById('descript').textContent = `Description: ${description}`
     })
+})
+
+
+
+document.getElementById('save-city').addEventListener('click', () => {
+  let cityElem = document.createElement('li')
+  cityElem.textContent = `${document.getElementById('city').textContent}`
+  document.getElementById('saved-cities-list').append(cityElem)
+  // local storage
+  let arrCities = JSON.parse(localStorage.getItem('cities')) || []
+  console.log(arrCities)
+  arrCities.push(cityElem.textContent)
+  localStorage.setItem('cities', JSON.stringify(arrCities))
+  
 })
